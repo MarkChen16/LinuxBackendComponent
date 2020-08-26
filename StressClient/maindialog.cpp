a3fdd0da79de85a8fb9ead1c3493e2d0db8c5035
Mark Chen <guiquanchen@qq.com>
2020-08-26 01:39:53 -0700

    add helloworld  stressclient

---
+++ StressClient/maindialog.cpp
@@ -0,0 +1,14 @@
+#include "maindialog.h"
+#include "ui_maindialog.h"
+
+mainDialog::mainDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::mainDialog)
+{
+    ui->setupUi(this);
+}
+
+mainDialog::~mainDialog()
+{
+    delete ui;
+}
