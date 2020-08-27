73e4cf83df931c475344ca231b8f48487a5d96b9
Mark Chen <guiquanchen@qq.com>
2020-08-27 03:21:10 -0700

    update stressclient

--- StressClient/mainwindow.cpp
+++ StressClient/mainwindow.cpp
@@ -1,11 +1,17 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QDesktopWidget>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    //居中显示
+    QDesktopWidget* deskdop = QApplication::desktop();
+    move((deskdop->width() - this->width())/2, (deskdop->height() - this->height())/2);
 }
 
 MainWindow::~MainWindow()
